d57cf39246ebef1c89a9f6c9fc939927fa19ad88
lordbad <lordbad@4f3def0e-ab49-11de-97f0-97815e7b2756>
2009-12-06 11:59:46 +0000

    - greatly improved dictionary handling by the database service

---
+++ core/src/main/java/com/drowltd/dictionary/core/db/Dictionary.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package com.drowltd.dictionary.core.db;
+
+/**
+ *
+ * @author bozhidar
+ */
+public enum Dictionary {
+    EN_BG, BG_EN;
+}
