f23be7e80c11cb118015ad630b72248c376eea63
strannika <strannika@4f3def0e-ab49-11de-97f0-97815e7b2756>
2009-12-27 10:09:47 +0000

    AboutDialog window is changed, license and credits information are added.

--- trunk/desktop/src/main/java/com/drowltd/dictionary/ui/desktop/AboutDialog.java
+++ trunk/desktop/src/main/java/com/drowltd/dictionary/ui/desktop/AboutDialog.java
@@ -8,12 +8,18 @@
  *
  * Created on Nov 7, 2009, 5:03:23 PM
  */
-
 package com.drowltd.dictionary.ui.desktop;
+
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.event.HyperlinkEvent;
 
 /**
  *
- * @author bozhidar
+ * @author Aleksandar Vulchev
  */
 public class AboutDialog extends javax.swing.JDialog {
 
@@ -21,6 +27,14 @@ public class AboutDialog extends javax.swing.JDialog {
     public AboutDialog(java.awt.Frame parent, boolean modal) {
         super(parent, modal);
         initComponents();
+
+        /** Checks whether desktop is supported and enable button that launch browser */
+        if (Desktop.isDesktopSupported()) {
+            if (Desktop.getDesktop().isSupported(Desktop.Action.BROWSE)) {
+                licenseButton.setEnabled(true);
+            }
+        }
+
     }
 
     /** This method is called from within the constructor to
@@ -32,27 +46,36 @@ public class AboutDialog extends javax.swing.JDialog {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        jLabel1 = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
         creditsButton = new javax.swing.JButton();
         licenseButton = new javax.swing.JButton();
         closeButton = new javax.swing.JButton();
-        jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
+        nameAndLogoPanel = new javax.swing.JPanel();
+        logoLabel = new javax.swing.JLabel();
+        spellbookLabel = new javax.swing.JLabel();
+        dictionaryLabel = new javax.swing.JLabel();
+        versionLabel = new javax.swing.JLabel();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        infoTextPane = new javax.swing.JTextPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         java.util.ResourceBundle bundle = java.util.ResourceBundle.getBundle("i18n/AboutDialog"); // NOI18N
         setTitle(bundle.getString("About(Title)")); // NOI18N
-
-        jLabel1.setFont(new java.awt.Font("DejaVu Sans", 0, 24)); // NOI18N
-        jLabel1.setText(bundle.getString("Spellbook(Label)")); // NOI18N
-
-        jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/logo/spellbook.png"))); // NOI18N
+        setResizable(false);
 
         creditsButton.setText("Credits");
+        creditsButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                creditsButtonActionPerformed(evt);
+            }
+        });
 
         licenseButton.setText("License");
+        licenseButton.setEnabled(false);
+        licenseButton.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                licenseButtonActionPerformed(evt);
+            }
+        });
 
         closeButton.setText("Close");
         closeButton.addActionListener(new java.awt.event.ActionListener() {
@@ -61,53 +84,99 @@ public class AboutDialog extends javax.swing.JDialog {
             }
         });
 
-        jLabel3.setFont(new java.awt.Font("DejaVu Sans", 0, 18)); // NOI18N
-        jLabel3.setText(bundle.getString("Version(Label)")); // NOI18N
+        nameAndLogoPanel.setBackground(new java.awt.Color(231, 243, 250));
 
-        jLabel4.setText(bundle.getString("License(Label)")); // NOI18N
+        logoLabel.setBackground(new java.awt.Color(255, 255, 255));
+        logoLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/logo/spellbook.png"))); // NOI18N
 
-        jLabel5.setText(bundle.getString("Copyright(Label)")); // NOI18N
+        spellbookLabel.setFont(new java.awt.Font("All Times New Roman", 0, 36));
+        spellbookLabel.setForeground(new java.awt.Color(0, 0, 102));
+        spellbookLabel.setText("Spellbook");
+
+        dictionaryLabel.setFont(new java.awt.Font("All Times New Roman", 0, 24));
+        dictionaryLabel.setForeground(new java.awt.Color(0, 0, 102));
+        dictionaryLabel.setText("Dictionary");
+
+        versionLabel.setFont(new java.awt.Font("All Times New Roman", 2, 14));
+        versionLabel.setForeground(new java.awt.Color(0, 0, 102));
+        versionLabel.setText("Version 0.1");
+
+        javax.swing.GroupLayout nameAndLogoPanelLayout = new javax.swing.GroupLayout(nameAndLogoPanel);
+        nameAndLogoPanel.setLayout(nameAndLogoPanelLayout);
+        nameAndLogoPanelLayout.setHorizontalGroup(
+            nameAndLogoPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(nameAndLogoPanelLayout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(logoLabel)
+                .addGap(18, 18, 18)
+                .addGroup(nameAndLogoPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(spellbookLabel)
+                    .addGroup(nameAndLogoPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
+                        .addComponent(versionLabel)
+                        .addComponent(dictionaryLabel)))
+                .addGap(36, 36, 36))
+        );
+        nameAndLogoPanelLayout.setVerticalGroup(
+            nameAndLogoPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(nameAndLogoPanelLayout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addGroup(nameAndLogoPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(nameAndLogoPanelLayout.createSequentialGroup()
+                        .addComponent(spellbookLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(dictionaryLabel)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(versionLabel))
+                    .addComponent(logoLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 109, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(28, Short.MAX_VALUE))
+        );
+
+        jScrollPane1.setBorder(null);
+
+        infoTextPane.setBackground(new java.awt.Color(224, 223, 227));
+        infoTextPane.setBorder(null);
+        infoTextPane.setContentType("text/html");
+        infoTextPane.setEditable(false);
+        infoTextPane.setFont(new java.awt.Font("All Times New Roman", 0, 11));
+        infoTextPane.setText("<html>\n  <head>\n\n  </head>\n  <body>\n    <p style=\"margin-top: 0\"; style=\"text-align:center\">\n      \t\n\tA platform independent dictionary written in Java.<br />\n\tThis program is free software licensed under GPL v3.<br />\n\tCopyright 2009 Drow Ltd. & Contributors\n    </p>\n  </body>\n</html>\n");
+        infoTextPane.addHyperlinkListener(new javax.swing.event.HyperlinkListener() {
+            public void hyperlinkUpdate(javax.swing.event.HyperlinkEvent evt) {
+                infoTextPaneHyperlinkUpdate(evt);
+            }
+        });
+        jScrollPane1.setViewportView(infoTextPane);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(nameAndLogoPanel, javax.swing.GroupLayout.DEFAULT_SIZE, 335, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(55, Short.MAX_VALUE)
+                .addComponent(creditsButton)
+                .addGap(18, 18, 18)
+                .addComponent(licenseButton)
+                .addGap(18, 18, 18)
+                .addComponent(closeButton)
+                .addGap(43, 43, 43))
             .addGroup(layout.createSequentialGroup()
-                .addGap(78, 78, 78)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
-                    .addGroup(layout.createSequentialGroup()
-                        .addComponent(creditsButton)
-                        .addGap(18, 18, 18)
-                        .addComponent(licenseButton)
-                        .addGap(18, 18, 18)
-                        .addComponent(closeButton))
-                    .addComponent(jLabel1)
-                    .addComponent(jLabel2)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel5))
-                .addContainerGap(25, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 315, Short.MAX_VALUE)
+                .addContainerGap())
         );
 
         layout.linkSize(javax.swing.SwingConstants.HORIZONTAL, new java.awt.Component[] {closeButton, creditsButton, licenseButton});
 
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jLabel1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel3)
-                .addGap(18, 18, 18)
-                .addComponent(jLabel4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jLabel5)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 29, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addComponent(nameAndLogoPanel, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 177, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(creditsButton)
                     .addComponent(licenseButton)
+                    .addComponent(creditsButton)
                     .addComponent(closeButton))
                 .addContainerGap())
         );
@@ -119,16 +188,52 @@ public class AboutDialog extends javax.swing.JDialog {
         setVisible(false);
     }//GEN-LAST:event_closeButtonActionPerformed
 
+    private void licenseButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_licenseButtonActionPerformed
+        URL license = AboutDialog.class.getResource("/gplv3/gpl.html");
+        try {
+            Desktop.getDesktop().browse(license.toURI());
+        } catch (URISyntaxException ex) {
+            Logger.getLogger(AboutDialog.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(AboutDialog.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_licenseButtonActionPerformed
 
+    private void creditsButtonActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_creditsButtonActionPerformed
+        infoTextPane.setText("<html>\n  <head>\n\n  </head>\n  <body>\n    <p style=\"margin-top: 0\"; style=\"text-align:center\">\n"
+                + "\t\n\t <a href=\"mailto:bozhidar@drowltd.com?subject=Spellbook\">Божидар Бацов</a> <br />"
+                + "\n\t <a href=\"mailto:mireflame@gmail.com?subject=Spellbook\">Мирослава Станчева</a> <br />"
+                + "\n\t <a href=\"mailto:nikolay@dimitrovi.net?subject=Spellbook\">Николай Димитров</a> <br />"
+                + "\n\t <a \nhref=\"mailto:frankeys89@gmail.com?subject=Spellbook\">Иван Спасов</a> <br />"
+                + "\n\t <a href=\"mailto:bmeshkova@gmail.com?subject=Spellbook\">Биляна Мешкова</a> <br />"
+                + "\n\t <a \nhref=\"mailto:cvetie@abv.bg?subject=Spellbook\">Цветелина Николова</a> <br />"
+                + "\n\t <a href=\"mailto:strannika@gmail.com?subject=Spellbook\">Александър Вълчев</a> <br />"
+                + "\n\t   \n\t</p>\n\t\n  </body>\n</html>\n");
+    }//GEN-LAST:event_creditsButtonActionPerformed
+
+    private void infoTextPaneHyperlinkUpdate(javax.swing.event.HyperlinkEvent evt) {//GEN-FIRST:event_infoTextPaneHyperlinkUpdate
+        if (evt.getEventType() == HyperlinkEvent.EventType.ACTIVATED) {
+            try {
+                try {
+                    Desktop.getDesktop().mail(evt.getURL().toURI());
+                } catch (IOException ex) {
+                    Logger.getLogger(AboutDialog.class.getName()).log(Level.SEVERE, null, ex);
+                }
+            } catch (URISyntaxException ex) {
+                Logger.getLogger(AboutDialog.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+    }//GEN-LAST:event_infoTextPaneHyperlinkUpdate
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton closeButton;
     private javax.swing.JButton creditsButton;
-    private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
-    private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel dictionaryLabel;
+    private javax.swing.JTextPane infoTextPane;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton licenseButton;
+    private javax.swing.JLabel logoLabel;
+    private javax.swing.JPanel nameAndLogoPanel;
+    private javax.swing.JLabel spellbookLabel;
+    private javax.swing.JLabel versionLabel;
     // End of variables declaration//GEN-END:variables
-
 }
