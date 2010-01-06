a53925e93de462ff4e11143d2e68181e481b593b
iivalchev <iivalchev@4f3def0e-ab49-11de-97f0-97815e7b2756>
2010-01-06 15:36:01 +0000

    New database abstraction layer interface, to fit with the new database structre.

---
+++ core/src/main/java/com/drowltd/dictionary/core/db/Language.java
@@ -0,0 +1,28 @@
+
+package com.drowltd.dictionary.core.db;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author iivalchev
+ */
+public class Language {
+
+    public Language(){
+
+    }
+
+    public String getName(){
+        return "";
+    }
+
+    public String getAlphabet(){
+        return "";
+    }
+
+    public ImageIcon getFlag(){
+        return new ImageIcon();
+    }
+
+}
