4e9dc8f6512eed89df5f6573b47a2dfae6891c44
lordbad <lordbad@4f3def0e-ab49-11de-97f0-97815e7b2756>
2010-01-26 19:46:47 +0000

    - fixed a minor bug

--- desktop/src/main/java/com/drowltd/dictionary/ui/desktop/exam/ExamDialog.java
+++ desktop/src/main/java/com/drowltd/dictionary/ui/desktop/exam/ExamDialog.java
@@ -31,7 +31,7 @@ public class ExamDialog extends javax.swing.JDialog {
     private int maximumWordsProgressBar = 0;
     private Dictionary selectedDictionary = Dictionary.EN_BG;
     private static Difficulty difficulty = Difficulty.EASY;
-    private ExamSettingsDialog examSettingsDialog = new ExamSettingsDialog(this, rootPaneCheckingEnabled);
+    private ExamSettingsDialog examSettingsDialog = new ExamSettingsDialog(this, true);
     
     private int totalWords;
     private int correctWords;
