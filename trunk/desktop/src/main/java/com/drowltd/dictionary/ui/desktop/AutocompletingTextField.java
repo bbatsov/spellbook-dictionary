ab80342f6b5824257a0a2b4e26529caee585673d
lordbad <lordbad@4f3def0e-ab49-11de-97f0-97815e7b2756>
2010-02-08 20:32:55 +0000

    - fixed a couple of regressions - made a few improvements to the autocompleting text field(which is still buggy as hell)

--- trunk/desktop/src/main/java/com/drowltd/dictionary/ui/desktop/AutocompletingTextField.java
+++ trunk/desktop/src/main/java/com/drowltd/dictionary/ui/desktop/AutocompletingTextField.java
@@ -1,25 +1,26 @@
 package com.drowltd.dictionary.ui.desktop;
 
-import java.awt.event.FocusAdapter;
-import java.awt.event.FocusEvent;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.util.ArrayList;
 import java.util.List;
 import javax.swing.DefaultListModel;
+import javax.swing.JFrame;
 import javax.swing.JList;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import javax.swing.JWindow;
 import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneConstants;
-import javax.swing.SwingUtilities;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class CompletableJTextField extends JTextField {
+public class AutocompletingTextField extends JTextField {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AutocompletingTextField.class);
 
     private Completer completer;
     private JList completionList;
@@ -27,34 +28,48 @@ public class CompletableJTextField extends JTextField {
     private JScrollPane listScroller;
     private JWindow listWindow;
     private JList wordsList;
+    private JFrame frame;
 
-    public CompletableJTextField() {
+    public AutocompletingTextField() {
         completer = new Completer();
 
         completionListModel = new DefaultListModel();
         completionList = new JList(completionListModel);
         completionList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
-        
-        completionList.addListSelectionListener(new ListSelectionListener() {
+
+        completionList.addKeyListener(new KeyAdapter() {
 
             @Override
-            public void valueChanged(ListSelectionEvent e) {
+            public void keyReleased(KeyEvent e) {
                 listWindow.setVisible(false);
 
                 final String completionString = (String) completionList.getSelectedValue();
 
-                Thread worker = new Thread() {
+                if (KeyEvent.VK_ENTER == e.getKeyCode()) {
+                    LOGGER.info("Enter press in completion list");
 
-                    @Override
-                    public void run() {
-                        if (completionString != null && !completionString.isEmpty()) {
-                            setText(completionString);
-                            wordsList.setSelectedValue(completionString, true);
-                        }
+                    if (completionString != null && !completionString.isEmpty()) {
+                        setText(completionString);
+                        wordsList.setSelectedValue(completionString, true);
                     }
-                };
+                }
+            }
+        });
 
-                SwingUtilities.invokeLater(worker);
+        completionList.addMouseListener(new MouseAdapter() {
+
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                LOGGER.info("Mouse pressed in completion list");
+
+                listWindow.setVisible(false);
+
+                final String completionString = (String) completionList.getSelectedValue();
+
+                if (completionString != null && !completionString.isEmpty()) {
+                    setText(completionString);
+                    wordsList.setSelectedValue(completionString, true);
+                }
             }
         });
 
@@ -62,27 +77,35 @@ public class CompletableJTextField extends JTextField {
                                        ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
                                        ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
 
-        listWindow = new JWindow();
+        listWindow = new JWindow(frame);
         listWindow.getContentPane().add(listScroller);
-
-        addFocusListener(new FocusAdapter() {
-
-            @Override
-            public void focusLost(FocusEvent e) {
-                listWindow.setVisible(false);
-            }
-        });
 
         addKeyListener(new KeyAdapter() {
 
             @Override
             public void keyReleased(KeyEvent e) {
                 if (KeyEvent.VK_ESCAPE == e.getKeyCode()) {
+                    LOGGER.info("Escape pressed in word search field");
+
                     listWindow.setVisible(false);
                 }
-            }
 
+                if (KeyEvent.VK_DOWN == e.getKeyCode()) {
+                    LOGGER.info("Down arrow pressed in word search field");
+
+                    listWindow.requestFocusInWindow();
+
+                    if (completionList.getModel().getSize() > 0) {
+                        completionList.requestFocusInWindow();
+                        completionList.setSelectedIndex(0);
+                    }
+                }
+            }
         });
+    }
+
+    public void setFrame(JFrame frame) {
+        this.frame = frame;
     }
 
     public void addCompletion(String s) {
@@ -99,6 +122,14 @@ public class CompletableJTextField extends JTextField {
 
     public void setWordsList(JList wordsList) {
         this.wordsList = wordsList;
+    }
+
+    public void showCompletions() {
+        completer.buildAndShowPopup();
+    }
+
+    public void hideCompletions() {
+        listWindow.setVisible(false);
     }
 
     /** inner class does the matching of the JTextField's
