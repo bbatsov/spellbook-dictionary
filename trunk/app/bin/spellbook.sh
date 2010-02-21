3b21139b0414918b94d31001eda9e076b3531527
lordbad <lordbad@4f3def0e-ab49-11de-97f0-97815e7b2756>
2010-02-21 23:29:23 +0000

    - updated startup script

--- trunk/app/bin/spellbook.sh
+++ trunk/app/bin/spellbook.sh
@@ -2,10 +2,14 @@
 
 SPELLBOOK_VERSION=0.3-SNAPSHOT
 
-# find the script dir
-script_dir=`dirname "$0"`
-# go to the script dir
-cd "$script_dir"
+if [ -e /opt/spellbook/dictionary-ui-0.3-SNAPSHOT-jar-with-dependencies.jar ]; then
+    java -jar /opt/spellbook/dictionary-ui-0.3-SNAPSHOT-jar-with-dependencies.jar
+else
+    # find the script dir
+    script_dir=`dirname "$0"`
+    # go to the script dir
+    cd "$script_dir"
 
-# run Spellbook's binary
-java -jar spellbook-dictionary-$SPELLBOOK_VERSION.jar
+    # run Spellbook's binary
+    java -jar dictionary-ui-0.3-SNAPSHOT-jar-with-dependencies.jar
+fi
