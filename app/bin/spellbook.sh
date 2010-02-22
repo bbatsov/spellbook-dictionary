96a24155a4458b178498ddf614ea67c053adc48d
lordbad <lordbad@4f3def0e-ab49-11de-97f0-97815e7b2756>
2010-02-22 06:13:02 +0000

    - tidied up a bit the startup script

--- app/bin/spellbook.sh
+++ app/bin/spellbook.sh
@@ -1,15 +1,19 @@
 #!/bin/sh
 
+DEFAULT_INSTALLATION_FOLDER=/opt/spellbook
 SPELLBOOK_VERSION=0.3-SNAPSHOT
+SPELLBOOK_JAR=dictionary-ui-$SPELLBOOK_VERSION-jar-with-dependencies.jar
 
-if [ -e /opt/spellbook/dictionary-ui-0.3-SNAPSHOT-jar-with-dependencies.jar ]; then
-    java -jar /opt/spellbook/dictionary-ui-0.3-SNAPSHOT-jar-with-dependencies.jar
+# first we check if spellbook is the default intallation location
+if [ -e $DEFAULT_INSTALLATION_FOLDER/$SPELLBOOK_JAR ]; then
+    java -jar $DEFAULT_INSTALLATION_FOLDER/$SPELLBOOK_JAR
 else
+    # otherwise the simple archive was probably used
     # find the script dir
     script_dir=`dirname "$0"`
     # go to the script dir
     cd "$script_dir"
 
     # run Spellbook's binary
-    java -jar dictionary-ui-0.3-SNAPSHOT-jar-with-dependencies.jar
+    java -jar $SPELLBOOK_JAR
 fi
