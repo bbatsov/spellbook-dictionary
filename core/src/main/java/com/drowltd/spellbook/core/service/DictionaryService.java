49f9cb8120ccab4f22d8fee8ab364a2820a4d37c
lordbad <lordbad@4f3def0e-ab49-11de-97f0-97815e7b2756>
2010-03-01 21:16:54 +0000

    - made the jdbc connection configurable

--- core/src/main/java/com/drowltd/spellbook/core/service/DictionaryService.java
+++ core/src/main/java/com/drowltd/spellbook/core/service/DictionaryService.java
@@ -4,8 +4,11 @@
  */
 package com.drowltd.spellbook.core.service;
 
+import com.drowltd.spellbook.core.exception.DictionaryDbLockedException;
 import com.drowltd.spellbook.core.model.Dictionary;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
 import org.slf4j.Logger;
@@ -17,7 +20,49 @@ import org.slf4j.LoggerFactory;
  */
 public class DictionaryService {
     private static final Logger LOGGER = LoggerFactory.getLogger(DictionaryService.class);
-    private static final EntityManager EM = Persistence.createEntityManagerFactory("Spellbook").createEntityManager();
+    private static EntityManager EM;
+    private static DictionaryService instance;
+
+    private DictionaryService(String dictDbFile) throws DictionaryDbLockedException {
+        LOGGER.info("dictionary database: " + dictDbFile.replace(".data.db", ""));
+
+        String url = "jdbc:h2:" + dictDbFile.replace(".data.db", "");
+
+        try {
+            // we need to override the db url from persistence.xml
+            Map<String, String> properties = new HashMap<String, String>();
+            properties.put("hibernate.connection.url", url);
+
+            EM = Persistence.createEntityManagerFactory("Spellbook", properties).createEntityManager();
+        } catch (javax.persistence.PersistenceException e) {
+            if (e.getMessage() != null) {
+                if (e.getMessage().contains("Cannot open connection")) {
+                    throw new DictionaryDbLockedException();
+                }
+            }
+
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Bootstraps the dictionary service. The method can be executed only once.
+     *
+     * @param dictDbFile the dictionary database file
+     *
+     * @throws DictionaryDbLockedException if another process is already using the db file
+     */
+    public static void init(String dictDbFile) throws DictionaryDbLockedException {
+        if (instance == null) {
+            instance = new DictionaryService(dictDbFile);
+        } else {
+            LOGGER.info("Dictionary service is already initialized");
+        }
+    }
+
+    public static DictionaryService getInstance() {
+        return instance;
+    }
 
     public List<Dictionary> getDictionaries() {
         return EM.createNamedQuery("Dictionary.getAllDictionaries").getResultList();
@@ -41,8 +86,7 @@ public class DictionaryService {
             // we start looking for approximate matches of the full search key, but if we fail - we start looking
             // for shorter matches
             do {
-                List<String> matches = EM.createQuery("select de.word from DictionaryEntry de where de.dictionary = :dictionary and de.word like :searchKey order by de.word asc")
-                        .setParameter("dictionary", dictionary).setParameter("searchKey", builder.append("%").toString()).getResultList();
+                List<String> matches = EM.createQuery("select de.word from DictionaryEntry de where de.dictionary = :dictionary and de.word like :searchKey order by de.word asc").setParameter("dictionary", dictionary).setParameter("searchKey", builder.append("%").toString()).getResultList();
 
                 if (matches.size() > 0) {
                     return matches.get(0);
