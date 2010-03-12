6e0a9bc5dffe9fda8bcc9ec3f38c5dcfbbefc760
lordbad <lordbad@4f3def0e-ab49-11de-97f0-97815e7b2756>
2010-03-12 14:26:34 +0000

    - added a few more attributes to Language

--- trunk/core/src/main/java/com/drowltd/spellbook/core/model/Language.java
+++ trunk/core/src/main/java/com/drowltd/spellbook/core/model/Language.java
@@ -1,20 +1,28 @@
 package com.drowltd.spellbook.core.model;
 
-import javax.persistence.Id;
-
 public enum Language {
-    BULGARIAN("абвгдежзийклмнопрстуфхцчшщъьюя"),
-    ENGLISH("abcdefghijklmnopqrstuvwxyz");
+    BULGARIAN("Bulgarian", "абвгдежзийклмнопрстуфхцчшщъьюя", "flag_bulgaria.png"),
+    ENGLISH("English", "abcdefghijklmnopqrstuvwxyz", "flag_great_britain.png");
 
+    private String name;
     private String alphabet;
+    private String iconName;
 
-    Language() {}
-
-    private Language(String alphabet) {
+    private Language(String name, String alphabet, String iconName) {
+        this.name = name;
         this.alphabet = alphabet;
+        this.iconName = iconName;
+    }
+
+    public String getName() {
+        return name;
     }
 
     public String getAlphabet() {
         return alphabet;
-    }    
+    }
+
+    public String getIconName() {
+        return iconName;
+    }
 }
