7138e83073f758a7e7db9b335a4c35a8391ff44b
george.angelow@gmail.com <george.angelow@gmail.com@4f3def0e-ab49-11de-97f0-97815e7b2756>
2010-04-10 00:19:11 +0000

    Some work on ExamResultDialog, translations added as well

--- trunk/desktop/src/main/java/com/drowltd/spellbook/ui/desktop/exam/ExamResult.java
+++ trunk/desktop/src/main/java/com/drowltd/spellbook/ui/desktop/exam/ExamResult.java
@@ -6,17 +6,8 @@ import com.drowltd.spellbook.core.preferences.PreferencesManager;
 import com.drowltd.spellbook.core.service.exam.ExamService;
 import com.drowltd.spellbook.ui.swing.util.IconManager;
 import com.drowltd.spellbook.ui.swing.util.SwingUtil;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import javax.swing.RowSorter;
-import javax.swing.SortOrder;
-import javax.swing.table.AbstractTableModel;
-import javax.swing.table.JTableHeader;
-import javax.swing.table.TableModel;
-import javax.swing.table.TableRowSorter;
 
 /**
  *
@@ -66,7 +57,7 @@ public class ExamResult extends javax.swing.JDialog {
         endLabel = new javax.swing.JLabel();
         seeWrongWords = new javax.swing.JButton();
         iconLabel = new javax.swing.JLabel();
-        jLabel2 = new javax.swing.JLabel();
+        result = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         scoreboardNameField = new javax.swing.JTextField();
@@ -90,7 +81,7 @@ public class ExamResult extends javax.swing.JDialog {
             }
         });
 
-        correctWordsLabel.setFont(new java.awt.Font("Tahoma", 1, 11));
+        correctWordsLabel.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         correctWordsLabel.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         java.util.ResourceBundle bundle1 = java.util.ResourceBundle.getBundle("i18n/ExamDialog"); // NOI18N
         correctWordsLabel.setText(bundle1.getString("CorrectWords(String)")); // NOI18N
@@ -99,14 +90,14 @@ public class ExamResult extends javax.swing.JDialog {
         correctWrodsResultLabel.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
         correctWrodsResultLabel.setVerticalTextPosition(javax.swing.SwingConstants.TOP);
 
-        wrongWordsLabel.setFont(new java.awt.Font("Tahoma", 1, 11));
+        wrongWordsLabel.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         wrongWordsLabel.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         wrongWordsLabel.setText(bundle1.getString("WrongWords(String)")); // NOI18N
 
         wrongWrodsResultLabel.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         wrongWrodsResultLabel.setVerticalAlignment(javax.swing.SwingConstants.BOTTOM);
 
-        successRateLabel.setFont(new java.awt.Font("Tahoma", 1, 11));
+        successRateLabel.setFont(new java.awt.Font("Tahoma", 1, 11)); // NOI18N
         successRateLabel.setHorizontalAlignment(javax.swing.SwingConstants.LEFT);
         successRateLabel.setText(bundle.getString("Success(Label)")); // NOI18N
 
@@ -126,8 +117,8 @@ public class ExamResult extends javax.swing.JDialog {
 
         iconLabel.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/48x48/bell2_green.png"))); // NOI18N
 
-        jLabel2.setFont(new java.awt.Font("Tahoma", 1, 14));
-        jLabel2.setText("Your Results:");
+        result.setFont(new java.awt.Font("Tahoma", 1, 14));
+        result.setText(bundle.getString("Result(JLable)")); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -137,41 +128,37 @@ public class ExamResult extends javax.swing.JDialog {
                 .addContainerGap()
                 .addComponent(okResultButton, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(seeWrongWords, javax.swing.GroupLayout.DEFAULT_SIZE, 143, Short.MAX_VALUE)
+                .addComponent(seeWrongWords, javax.swing.GroupLayout.PREFERRED_SIZE, 143, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                .addGap(80, 80, 80)
-                .addComponent(jLabel2)
-                .addGap(78, 78, 78))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(10, 10, 10)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(correctWordsLabel)
-                    .addComponent(wrongWordsLabel)
-                    .addComponent(successRateLabel))
-                .addGap(28, 28, 28)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(successRateLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 100, Short.MAX_VALUE)
+                    .addComponent(wrongWordsLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(correctWordsLabel, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(10, 10, 10)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(wrongWrodsResultLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 52, Short.MAX_VALUE)
-                            .addGroup(jPanel1Layout.createSequentialGroup()
-                                .addComponent(correctWrodsResultLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)))
-                        .addComponent(iconLabel)
-                        .addGap(30, 30, 30))
-                    .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addComponent(successRateResultLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
-                        .addContainerGap())))
+                            .addComponent(wrongWrodsResultLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 52, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(correctWrodsResultLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 27, javax.swing.GroupLayout.PREFERRED_SIZE))
+                        .addComponent(iconLabel))
+                    .addComponent(successRateResultLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(30, 30, 30))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
-                .addComponent(endLabel, javax.swing.GroupLayout.DEFAULT_SIZE, 230, Short.MAX_VALUE)
+                .addComponent(endLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 230, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                .addContainerGap(80, Short.MAX_VALUE)
+                .addComponent(result, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(49, 49, 49))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addGap(4, 4, 4)
-                .addComponent(jLabel2)
+                .addComponent(result)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
@@ -179,16 +166,16 @@ public class ExamResult extends javax.swing.JDialog {
                         .addComponent(iconLabel))
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(correctWrodsResultLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
-                            .addComponent(correctWordsLabel))
+                            .addComponent(correctWordsLabel)
+                            .addComponent(correctWrodsResultLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(18, 18, 18)
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(wrongWordsLabel)
-                            .addComponent(wrongWrodsResultLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                            .addComponent(wrongWrodsResultLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 14, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(wrongWordsLabel))))
                 .addGap(9, 9, 9)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(successRateLabel)
-                    .addComponent(successRateResultLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(successRateResultLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(successRateLabel))
                 .addGap(21, 21, 21)
                 .addComponent(endLabel, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
@@ -200,7 +187,7 @@ public class ExamResult extends javax.swing.JDialog {
 
         jPanel2.setBorder(javax.swing.BorderFactory.createEtchedBorder());
 
-        jLabel1.setText("Enter your name:");
+        jLabel1.setText(bundle.getString("ScoreboardName(JTextFiedl)")); // NOI18N
 
         scoreboardNameField.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -208,7 +195,7 @@ public class ExamResult extends javax.swing.JDialog {
             }
         });
 
-        submitScoreButton.setText("Submit Score");
+        submitScoreButton.setText(bundle.getString("SubmitScore(JButton)")); // NOI18N
         submitScoreButton.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 submitScoreButtonActionPerformed(evt);
@@ -217,7 +204,7 @@ public class ExamResult extends javax.swing.JDialog {
 
         scoreboardButton.setForeground(new java.awt.Color(0, 0, 255));
         scoreboardButton.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
-        scoreboardButton.setText("Show Scoreboard");
+        scoreboardButton.setText(bundle.getString("ShowScoreboard(JLabel)")); // NOI18N
         scoreboardButton.setCursor(new java.awt.Cursor(java.awt.Cursor.HAND_CURSOR));
         scoreboardButton.setHorizontalTextPosition(javax.swing.SwingConstants.RIGHT);
         scoreboardButton.addMouseListener(new java.awt.event.MouseAdapter() {
@@ -232,15 +219,15 @@ public class ExamResult extends javax.swing.JDialog {
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addContainerGap()
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(scoreboardNameField, javax.swing.GroupLayout.PREFERRED_SIZE, 101, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                         .addComponent(submitScoreButton, javax.swing.GroupLayout.PREFERRED_SIZE, 123, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addGroup(jPanel2Layout.createSequentialGroup()
                         .addComponent(jLabel1)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 62, Short.MAX_VALUE)
-                        .addComponent(scoreboardButton, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(scoreboardButton, javax.swing.GroupLayout.PREFERRED_SIZE, 98, javax.swing.GroupLayout.PREFERRED_SIZE)))
                 .addContainerGap())
         );
         jPanel2Layout.setVerticalGroup(
@@ -252,9 +239,9 @@ public class ExamResult extends javax.swing.JDialog {
                         .addComponent(jLabel1))
                     .addComponent(scoreboardButton))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                .addGroup(jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(submitScoreButton)
-                    .addComponent(scoreboardNameField, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(scoreboardNameField, javax.swing.GroupLayout.PREFERRED_SIZE, 22, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
@@ -371,7 +358,7 @@ public class ExamResult extends javax.swing.JDialog {
         if (!scoreboardNameField.getText().isEmpty()) {
             scoreboardFilling();
         } else {
-            SwingUtil.showBalloonTip(scoreboardNameField, "Please enter a name");
+            SwingUtil.showBalloonTip(scoreboardNameField, TRANSLATOR.translate("EnterName(BallonTip)"));
         }
     }//GEN-LAST:event_submitScoreButtonActionPerformed
 
@@ -379,26 +366,23 @@ public class ExamResult extends javax.swing.JDialog {
         if (!scoreboardNameField.getText().isEmpty()) {
             scoreboardFilling();
         } else {
-            SwingUtil.showBalloonTip(scoreboardNameField, "Please enter a name");
+            SwingUtil.showBalloonTip(scoreboardNameField, TRANSLATOR.translate("EnterName(BallonTip)"));
         }
     }//GEN-LAST:event_scoreboardNameFieldActionPerformed
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel correctWordsLabel;
     private javax.swing.JLabel correctWrodsResultLabel;
     private javax.swing.JLabel endLabel;
-    private javax.persistence.EntityManager entityManager;
     private javax.swing.JLabel iconLabel;
     private javax.swing.JLabel jLabel1;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JButton okResultButton;
+    private javax.swing.JLabel result;
     private javax.swing.JTable scoreboarTable;
     private javax.swing.JLabel scoreboardButton;
-    private java.util.List<com.drowltd.spellbook.core.model.ScoreboardEntry> scoreboardEntryList;
-    private javax.persistence.Query scoreboardEntryQuery;
     private javax.swing.JTextField scoreboardNameField;
     private javax.swing.JButton seeWrongWords;
     private javax.swing.JButton submitScoreButton;
@@ -410,7 +394,7 @@ public class ExamResult extends javax.swing.JDialog {
 
     public void showExamResult(int correctWords, int totalWords) {
 
-        setSize(279, 353);
+        setSize(279, 342);
 
         int uspeh = ((correctWords * 100) / totalWords);
 
@@ -431,13 +415,13 @@ public class ExamResult extends javax.swing.JDialog {
     public void scoreboardViewStatus() {
 
         if (scoreboardVisibility == false) {
-            scoreboardButton.setText("Hide Scoreboard");
+            scoreboardButton.setText(TRANSLATOR.translate("HideScoreboard(JLabel)"));
             scoreboardVisibility = true;
             setTable();
-            setSize(544, 353);
+            setSize(544, 342);
         } else {
-            scoreboardButton.setText("See Scoreboard");
-            setSize(279, 353);
+            scoreboardButton.setText(TRANSLATOR.translate("ShowScoreboard(JLabel)"));
+            setSize(279, 342);
             scoreboardVisibility = false;
         }
 
