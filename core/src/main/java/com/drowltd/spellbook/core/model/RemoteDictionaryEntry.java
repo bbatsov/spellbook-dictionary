7b7f4c4f67219c2cf09399e8b90a8a81ea15cf73
iivalchev <iivalchev@4f3def0e-ab49-11de-97f0-97815e7b2756>
2010-05-01 16:49:58 +0000

    revisions for RemoteDictionaryEntry translations added

--- core/src/main/java/com/drowltd/spellbook/core/model/RemoteDictionaryEntry.java
+++ core/src/main/java/com/drowltd/spellbook/core/model/RemoteDictionaryEntry.java
@@ -1,9 +1,14 @@
 package com.drowltd.spellbook.core.model;
 
+import java.util.HashSet;
+import java.util.Set;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 /**
@@ -12,26 +17,22 @@ import javax.persistence.Table;
  */
 @Entity(name = "RemoteDictionaryEntry")
 @Table(name = "REMOTE_DICTIONARY_ENTRY")
+@NamedQueries({
+@NamedQuery(name = "RemoteDictionaryEntry.getLastRevision",
+query = "select re from RevisionEntry re where re.created = (select max(re.created) from re) and re.remoteDictionaryEntry = :remoteDictionaryEntry"),
+@NamedQuery(name="RemoteDictionaryEntry.getRemoteDictionaryEntry", query="select rde from RemoteDictionaryEntry rde where rde.word = :word")})
 public class RemoteDictionaryEntry extends AbstractEntity {
 
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     private RemoteDictionary remoteDictionary;
-    @Column(nullable = false, unique = true)
+
+    @Column(name="word", nullable = false, unique = true)
     private String word;
-    @Column(name = "word_translation", nullable = false, length = 10000)
-    private String translation;
-    @ManyToOne(optional = false, fetch = FetchType.LAZY)
-    private UpdateEntry updateEntry;
+
+    @OneToMany(mappedBy = "remoteDictionaryEntry", fetch = FetchType.LAZY)
+    private Set<RevisionEntry> revisions = new HashSet<RevisionEntry>();
 
     public RemoteDictionaryEntry() {
-    }
-
-    public UpdateEntry getUpdateEntry() {
-        return updateEntry;
-    }
-
-    public void setUpdateEntry(UpdateEntry updateEntry) {
-        this.updateEntry = updateEntry;
     }
 
     public RemoteDictionary getRemoteDictionary() {
@@ -42,13 +43,20 @@ public class RemoteDictionaryEntry extends AbstractEntity {
         this.remoteDictionary = remoteDictionary;
     }
 
-
-    public String getTranslation() {
-        return translation;
+    public Set<RevisionEntry> getRevisions() {
+        return revisions;
     }
 
-    public void setTranslation(String translation) {
-        this.translation = translation;
+    public void setRevisions(Set<RevisionEntry> revisions) {
+        this.revisions = revisions;
+    }
+
+    public void addRevision(RevisionEntry entry) {
+        if (entry == null) {
+            return;
+        }
+
+        revisions.add(entry);
     }
 
     public String getWord() {
@@ -58,22 +66,5 @@ public class RemoteDictionaryEntry extends AbstractEntity {
     public void setWord(String word) {
         this.word = word;
     }
-
-    public DictionaryEntry toDictionaryEntry() {
-        DictionaryEntry de = new DictionaryEntry();
-        de.setAddedByUser(false);
-        de.setWord(this.getWord());
-        de.setTranslation(this.getTranslation());
-
-        return de;
-    }
-
-    public RankEntry toRankEntry(){
-        RankEntry re = new RankEntry();
-        re.setWord(word);
-        re.setRank(1);
-        re.setLanguage(remoteDictionary.getFromLanguage());
-
-        return re;
-    }
+    
 }
