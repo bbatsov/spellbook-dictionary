d3c0490e8299d474cec5c358a189b1af39999fcb
lordbad <lordbad@4f3def0e-ab49-11de-97f0-97815e7b2756>
2010-05-05 06:18:11 +0000

    - code style

--- desktop/src/main/java/com/drowltd/spellbook/ui/desktop/spellcheck/SpellCheckFrame.java
+++ desktop/src/main/java/com/drowltd/spellbook/ui/desktop/spellcheck/SpellCheckFrame.java
@@ -5,36 +5,50 @@ import com.drowltd.spellbook.core.model.Language;
 import com.drowltd.spellbook.core.service.DictionaryService;
 import com.drowltd.spellbook.core.spellcheck.SpellChecker;
 import com.drowltd.spellbook.ui.swing.util.IconManager;
+import net.miginfocom.swing.MigLayout;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.accessibility.AccessibleEditableText;
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPopupMenu;
+import javax.swing.JScrollPane;
+import javax.swing.JTextPane;
+import javax.swing.JViewport;
+import javax.swing.KeyStroke;
+import javax.swing.SwingConstants;
+import javax.swing.Timer;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.BadLocationException;
+import javax.swing.undo.UndoManager;
+import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.AdjustmentEvent;
 import java.awt.event.AdjustmentListener;
+import java.awt.event.InputEvent;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.List;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
-import javax.accessibility.AccessibleEditableText;
-import javax.swing.JMenuItem;
-import javax.swing.JTextPane;
-import javax.swing.JViewport;
-import javax.swing.Timer;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
-import javax.swing.text.BadLocationException;
-import javax.swing.undo.UndoManager;
-import net.miginfocom.swing.MigLayout;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  *
  * @author iivalchev
  * @since 0.2
  */
-public class SpellCheckFrame extends javax.swing.JFrame implements StatusManager.StatusObserver {
+public class SpellCheckFrame extends JFrame implements StatusManager.StatusObserver {
 
     private static final SpellCheckFrame INSTANCE = new SpellCheckFrame();
     private static final Logger LOGGER = LoggerFactory.getLogger(SpellCheckFrame.class);
@@ -43,24 +57,13 @@ public class SpellCheckFrame extends javax.swing.JFrame implements StatusManager
     private SpellCheckHighlighter checkHighlighter;
     private Timer documentChangedTimer;
     private Timer adjustmentValueTimer;
-    private final int INTERVAL = 550;
     private Language selectedLanguage = Language.ENGLISH;
     //components
-    private javax.swing.JMenuItem jCopyMenuItem;
-    private javax.swing.JMenuItem jCutMenuItem;
-    private javax.swing.JMenu jDictionaryMenu;
-    private javax.swing.JMenuItem jExitMenuItem;
-    private javax.swing.JLabel jLanguageLabel;
-    private javax.swing.JMenu jMenu1;
-    private javax.swing.JMenu jMenu2;
-    private javax.swing.JMenuBar jMenuBar1;
-    private javax.swing.JMenuItem jPasteMenuItem;
-    private javax.swing.JMenuItem jRedoMenuItem;
-    private javax.swing.JScrollPane jScrollPane;
-    private javax.swing.JPopupMenu.Separator jSeparator1;
-    private javax.swing.JLabel jStatusLabel;
-    private javax.swing.JTextPane jTextPane;
-    private javax.swing.JMenuItem jUndoMenuItem;
+    private JMenu jDictionaryMenu;
+    private JLabel jLanguageLabel;
+    private JScrollPane jScrollPane;
+    private JLabel jStatusLabel;
+    private JTextPane jTextPane;
 
     public static SpellCheckFrame getInstance() {
         return INSTANCE;
@@ -74,52 +77,52 @@ public class SpellCheckFrame extends javax.swing.JFrame implements StatusManager
     }
 
     private void initComponents0() {
-        jScrollPane = new javax.swing.JScrollPane();
-        jTextPane = new javax.swing.JTextPane();
-        jStatusLabel = new javax.swing.JLabel();
-        jLanguageLabel = new javax.swing.JLabel();
-        jMenuBar1 = new javax.swing.JMenuBar();
-        jMenu1 = new javax.swing.JMenu();
-        jExitMenuItem = new javax.swing.JMenuItem();
-        jMenu2 = new javax.swing.JMenu();
-        jUndoMenuItem = new javax.swing.JMenuItem();
-        jRedoMenuItem = new javax.swing.JMenuItem();
-        jSeparator1 = new javax.swing.JPopupMenu.Separator();
-        jCutMenuItem = new javax.swing.JMenuItem();
-        jCopyMenuItem = new javax.swing.JMenuItem();
-        jPasteMenuItem = new javax.swing.JMenuItem();
-        jDictionaryMenu = new javax.swing.JMenu();
+        jScrollPane = new JScrollPane();
+        jTextPane = new JTextPane();
+        jStatusLabel = new JLabel();
+        jLanguageLabel = new JLabel();
+        JMenuBar jMenuBar1 = new JMenuBar();
+        JMenu jMenu1 = new JMenu();
+        JMenuItem jExitMenuItem = new JMenuItem();
+        JMenu jMenu2 = new JMenu();
+        JMenuItem jUndoMenuItem = new JMenuItem();
+        JMenuItem jRedoMenuItem = new JMenuItem();
+        JPopupMenu.Separator jSeparator1 = new JPopupMenu.Separator();
+        JMenuItem jCutMenuItem = new JMenuItem();
+        JMenuItem jCopyMenuItem = new JMenuItem();
+        JMenuItem jPasteMenuItem = new JMenuItem();
+        jDictionaryMenu = new JMenu();
 
         setTitle("SpellBook SpellChecker");
 
-        jTextPane.setBackground(java.awt.Color.white);
-        jTextPane.addMouseListener(new java.awt.event.MouseAdapter() {
+        jTextPane.setBackground(Color.white);
+        jTextPane.addMouseListener(new MouseAdapter() {
 
             @Override
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
+            public void mouseClicked(MouseEvent evt) {
                 jTextPaneMouseClicked(evt);
             }
         });
         jScrollPane.setViewportView(jTextPane);
 
-        jLanguageLabel.setHorizontalAlignment(javax.swing.SwingConstants.RIGHT);
+        jLanguageLabel.setHorizontalAlignment(SwingConstants.RIGHT);
         jLanguageLabel.setText("l");
-        jLanguageLabel.addMouseListener(new java.awt.event.MouseAdapter() {
+        jLanguageLabel.addMouseListener(new MouseAdapter() {
 
             @Override
-            public void mouseClicked(java.awt.event.MouseEvent evt) {
+            public void mouseClicked(MouseEvent evt) {
                 jLanguageLabelMouseClicked(evt);
             }
         });
 
         jMenu1.setText("File");
 
-        jExitMenuItem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/16x16/exit.png"))); // NOI18N
+        jExitMenuItem.setIcon(new ImageIcon(getClass().getResource("/icons/16x16/exit.png"))); // NOI18N
         jExitMenuItem.setText("Exit");
-        jExitMenuItem.addActionListener(new java.awt.event.ActionListener() {
+        jExitMenuItem.addActionListener(new ActionListener() {
 
             @Override
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+            public void actionPerformed(ActionEvent evt) {
                 jExitMenuItemActionPerformed(evt);
             }
         });
@@ -129,58 +132,58 @@ public class SpellCheckFrame extends javax.swing.JFrame implements StatusManager
 
         jMenu2.setText("Edit");
 
-        jUndoMenuItem.setAccelerator(javax.swing.KeyStroke.getKeyStroke(java.awt.event.KeyEvent.VK_Z, java.awt.event.InputEvent.CTRL_MASK));
-        jUndoMenuItem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/16x16/undo.png"))); // NOI18N
+        jUndoMenuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.CTRL_MASK));
+        jUndoMenuItem.setIcon(new ImageIcon(getClass().getResource("/icons/16x16/undo.png"))); // NOI18N
         jUndoMenuItem.setText("Undo");
-        jUndoMenuItem.addActionListener(new java.awt.event.ActionListener() {
+        jUndoMenuItem.addActionListener(new ActionListener() {
 
             @Override
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+            public void actionPerformed(ActionEvent evt) {
                 jUndoMenuItemActionPerformed(evt);
             }
         });
         jMenu2.add(jUndoMenuItem);
 
-        jRedoMenuItem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/16x16/redo.png"))); // NOI18N
+        jRedoMenuItem.setIcon(new ImageIcon(getClass().getResource("/icons/16x16/redo.png"))); // NOI18N
         jRedoMenuItem.setText("Redo");
-        jRedoMenuItem.addActionListener(new java.awt.event.ActionListener() {
+        jRedoMenuItem.addActionListener(new ActionListener() {
 
             @Override
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+            public void actionPerformed(ActionEvent evt) {
                 jRedoMenuItemActionPerformed(evt);
             }
         });
         jMenu2.add(jRedoMenuItem);
         jMenu2.add(jSeparator1);
 
-        jCutMenuItem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/16x16/cut.png"))); // NOI18N
+        jCutMenuItem.setIcon(new ImageIcon(getClass().getResource("/icons/16x16/cut.png"))); // NOI18N
         jCutMenuItem.setText("Cut");
-        jCutMenuItem.addActionListener(new java.awt.event.ActionListener() {
+        jCutMenuItem.addActionListener(new ActionListener() {
 
             @Override
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+            public void actionPerformed(ActionEvent evt) {
                 jCutMenuItemActionPerformed(evt);
             }
         });
         jMenu2.add(jCutMenuItem);
 
-        jCopyMenuItem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/16x16/copy.png"))); // NOI18N
+        jCopyMenuItem.setIcon(new ImageIcon(getClass().getResource("/icons/16x16/copy.png"))); // NOI18N
         jCopyMenuItem.setText("Copy");
-        jCopyMenuItem.addActionListener(new java.awt.event.ActionListener() {
+        jCopyMenuItem.addActionListener(new ActionListener() {
 
             @Override
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+            public void actionPerformed(ActionEvent evt) {
                 jCopyMenuItemActionPerformed(evt);
             }
         });
         jMenu2.add(jCopyMenuItem);
 
-        jPasteMenuItem.setIcon(new javax.swing.ImageIcon(getClass().getResource("/icons/16x16/paste.png"))); // NOI18N
+        jPasteMenuItem.setIcon(new ImageIcon(getClass().getResource("/icons/16x16/paste.png"))); // NOI18N
         jPasteMenuItem.setText("Paste");
-        jPasteMenuItem.addActionListener(new java.awt.event.ActionListener() {
+        jPasteMenuItem.addActionListener(new ActionListener() {
 
             @Override
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
+            public void actionPerformed(ActionEvent evt) {
                 jPasteMenuItemActionPerformed(evt);
             }
         });
@@ -205,42 +208,42 @@ public class SpellCheckFrame extends javax.swing.JFrame implements StatusManager
 
     }
 
-    private void jTextPaneMouseClicked(java.awt.event.MouseEvent evt) {
+    private void jTextPaneMouseClicked(MouseEvent evt) {
         if (evt.getButton() == MouseEvent.BUTTON3) {
             popupMenu.show(evt);
         }
         //popupMenu.show(evt);
     }
 
-    private void jUndoMenuItemActionPerformed(java.awt.event.ActionEvent evt) {
+    private void jUndoMenuItemActionPerformed(ActionEvent evt) {
         if (undoManager.canUndo()) {
             undoManager.undo();
         }
     }
 
-    private void jRedoMenuItemActionPerformed(java.awt.event.ActionEvent evt) {
+    private void jRedoMenuItemActionPerformed(ActionEvent evt) {
         if (undoManager.canRedo()) {
             undoManager.redo();
         }
     }
 
-    private void jCutMenuItemActionPerformed(java.awt.event.ActionEvent evt) {
+    private void jCutMenuItemActionPerformed(ActionEvent evt) {
         jTextPane.cut();
     }
 
-    private void jCopyMenuItemActionPerformed(java.awt.event.ActionEvent evt) {
+    private void jCopyMenuItemActionPerformed(ActionEvent evt) {
         jTextPane.copy();
     }
 
-    private void jPasteMenuItemActionPerformed(java.awt.event.ActionEvent evt) {
+    private void jPasteMenuItemActionPerformed(ActionEvent evt) {
         jTextPane.paste();
     }
 
-    private void jExitMenuItemActionPerformed(java.awt.event.ActionEvent evt) {
+    private void jExitMenuItemActionPerformed(ActionEvent evt) {
         this.setVisible(false);
     }
 
-    private void jLanguageLabelMouseClicked(java.awt.event.MouseEvent evt) {
+    private void jLanguageLabelMouseClicked(MouseEvent evt) {
     }
 
     /**
@@ -288,6 +291,7 @@ public class SpellCheckFrame extends javax.swing.JFrame implements StatusManager
             }
         });
 
+        int INTERVAL = 550;
         documentChangedTimer = new Timer(INTERVAL, new ActionListener() {
 
             @Override
@@ -378,8 +382,8 @@ public class SpellCheckFrame extends javax.swing.JFrame implements StatusManager
      * @return VisibleText repersentig currently visible text
      */
     public VisibleText getVisibleText() {
-        int offset = 0;
-        int length = 0;
+        int offset;
+        int length;
 
         JViewport viewPort = (JViewport) jTextPane.getParent();
         offset = jTextPane.viewToModel(viewPort.getViewRect().getLocation());
