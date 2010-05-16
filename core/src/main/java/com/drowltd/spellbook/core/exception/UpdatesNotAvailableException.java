fc45eb5f9b798e6500068ad7526d1fbdd67677b6
iivalchev <iivalchev@4f3def0e-ab49-11de-97f0-97815e7b2756>
2010-05-16 13:49:47 +0000

    all of the update/commit functionality has been moved to a single Dialog

---
+++ core/src/main/java/com/drowltd/spellbook/core/exception/UpdatesNotAvailableException.java
@@ -0,0 +1,13 @@
+package com.drowltd.spellbook.core.exception;
+
+/**
+ * @author ikkari
+ *         Date: May 16, 2010
+ *         Time: 3:29:53 PM
+ */
+public class UpdatesNotAvailableException extends Exception{
+
+    public UpdatesNotAvailableException(String message){
+        super(message);
+    }
+}
