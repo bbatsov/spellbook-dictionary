af3553c28003237bea4b702a382dc5c50804fdad
lordbad <lordbad@4f3def0e-ab49-11de-97f0-97815e7b2756>
2010-06-15 22:59:16 +0000

    - hangman game(work in progress)

--- trunk/swing/src/main/java/com/drowltd/spellbook/ui/swing/component/BaseDialog.java
+++ trunk/swing/src/main/java/com/drowltd/spellbook/ui/swing/component/BaseDialog.java
@@ -4,6 +4,8 @@ import com.jidesoft.dialog.ButtonPanel;
 import com.jidesoft.dialog.StandardDialog;
 
 import javax.swing.JComponent;
+import java.awt.Frame;
+import java.awt.HeadlessException;
 
 /**
  * Base dialog class in Spellbook.
@@ -12,6 +14,13 @@ import javax.swing.JComponent;
  * @since 0.4
  */
 public class BaseDialog extends StandardDialog {
+    protected Frame parent;
+
+    public BaseDialog(Frame owner, boolean modal) throws HeadlessException {
+        super(owner, modal);
+        this.parent = owner;
+    }
+
     @Override
     public JComponent createBannerPanel() {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
@@ -25,5 +34,14 @@ public class BaseDialog extends StandardDialog {
     @Override
     public ButtonPanel createButtonPanel() {
         return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    public void showDialog() {
+        pack();
+
+        // this should be called after pack()!
+        setLocationRelativeTo(parent);
+
+        setVisible(true);
     }
 }
