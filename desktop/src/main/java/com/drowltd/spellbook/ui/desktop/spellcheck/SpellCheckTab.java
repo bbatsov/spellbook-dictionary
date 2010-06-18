02e930db72d8f8ed35c03e323d8d368a6216f1bb
iivalchev <iivalchev@4f3def0e-ab49-11de-97f0-97815e7b2756>
2010-06-18 14:03:04 +0000

    - some work done in adding tabs to the spellchecker

--- desktop/src/main/java/com/drowltd/spellbook/ui/desktop/spellcheck/SpellCheckTab.java
+++ desktop/src/main/java/com/drowltd/spellbook/ui/desktop/spellcheck/SpellCheckTab.java
@@ -6,7 +6,6 @@ import com.drowltd.spellbook.core.model.*;
 import com.drowltd.spellbook.core.service.*;
 import com.drowltd.spellbook.core.spellcheck.*;
 import com.drowltd.spellbook.ui.swing.component.*;
-import com.drowltd.spellbook.ui.swing.util.*;
 import org.slf4j.*;
 
 import javax.accessibility.*;
@@ -21,21 +20,40 @@ import java.util.concurrent.*;
 import java.util.regex.*;
 
 
-public class SpellCheckTab extends JPanel implements SpellCheckFrame.SpellCheckable {
+public class SpellCheckTab extends JPanel {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(SpellCheckTab.class);
     private static final ExecutorService executor = Executors.newCachedThreadPool();
     private static final Map<Language, SpellChecker> spellCheckersMap = new HashMap<Language, SpellChecker>();
     private static final Translator TRANSLATOR = Translator.getTranslator("SpellCheckTab");
+    private static final Set<String> userMisspelledSet = new HashSet<String>();
 
-    private FileTextPane textPane;
+    private FileTextPane fileTextPane;
     private UndoManager undoManager = new UndoManager();
     private SpellCheckPopupMenu popupMenu;
     private Language selectedLanguage = Language.ENGLISH;
     private Future<?> spellCheckTask;
+    private Highlighter highlighter = fileTextPane.getHighlighter();
+    private final javax.swing.text.Highlighter.HighlightPainter painter = new UnderlineHighlightPainter(Color.red);
 
     private final Map<String, MisspelledWord> misspelledMap = new ConcurrentHashMap<String, MisspelledWord>();
-    private final Set<String> userMisspelledSet = new HashSet<String>();
+
+    public SpellCheckTab(File file) throws IOException {
+        fileTextPane = new FileTextPane(file);
+        init();
+    }
+
+    public SpellCheckTab() {
+        fileTextPane = new FileTextPane();
+        init();
+    }
+
+    private void init() {
+        setLayout(new BorderLayout());
+        fileTextPane.getDocument().addUndoableEditListener(undoManager);
+        add(fileTextPane, BorderLayout.CENTER);
+    }
+
 
     public void correct(String correction, MisspelledWord misspelledWord, int cursorPosition) {
         if (correction == null) {
@@ -67,11 +85,11 @@ public class SpellCheckTab extends JPanel implements SpellCheckFrame.SpellChecka
             }
         }
 
-        final AccessibleEditableText editableText = textPane.getAccessibleContext().getAccessibleEditableText();
+        final AccessibleEditableText editableText = fileTextPane.getAccessibleContext().getAccessibleEditableText();
 
         Pattern p = Pattern.compile("\\b" + misspelledWord.getWord() + "\\b");
         do {
-            Matcher m = p.matcher(textPane.getText());
+            Matcher m = p.matcher(fileTextPane.getText());
             if (!m.find()) {
                 break;
             }
@@ -88,7 +106,7 @@ public class SpellCheckTab extends JPanel implements SpellCheckFrame.SpellChecka
         }
 
         if (cursorPosition > -1) {
-            textPane.setCaretPosition(cursorPosition);
+            fileTextPane.setCaretPosition(cursorPosition);
         }
 
         StatusManager.getInstance().setStatus(misspelledWord.getWord() + " corrected with " + correction);
@@ -105,14 +123,18 @@ public class SpellCheckTab extends JPanel implements SpellCheckFrame.SpellChecka
                 spellCheckersMap.put(language, spellChecker);
             } catch (SpellCheckerException e) {
                 LOGGER.error(e.getMessage(), e);
-                JOptionPane.showMessageDialog(this, TRANSLATOR.translate("Message(Content)"), TRANSLATOR.translate("Message(Title)"), JOptionPane.INFORMATION_MESSAGE);
+                showMessage(TRANSLATOR.translate("Message(Content)"), TRANSLATOR.translate("Message(Title)"));
             }
         }
 
         return spellChecker;
     }
 
-    @Override
+    private void showMessage(String content, String title) {
+        JOptionPane.showMessageDialog(this, content, title, JOptionPane.INFORMATION_MESSAGE);
+    }
+
+
     public List<String> getCorrections(MisspelledWord misspelledWord) {
         if (misspelledWord == null) {
             throw new IllegalArgumentException("misspelledWord is null");
@@ -120,7 +142,7 @@ public class SpellCheckTab extends JPanel implements SpellCheckFrame.SpellChecka
         return getSpellChecker(selectedLanguage).correct(misspelledWord.getWord());
     }
 
-    @Override
+
     public boolean misspelled(String word) {
         if (word == null) {
             return false;
@@ -136,7 +158,7 @@ public class SpellCheckTab extends JPanel implements SpellCheckFrame.SpellChecka
         return getSpellChecker(selectedLanguage).misspelled(word);
     }
 
-    @Override
+
     public void spellCheck(boolean clear) {
         if (clear) {
             misspelledMap.clear();
@@ -195,7 +217,7 @@ public class SpellCheckTab extends JPanel implements SpellCheckFrame.SpellChecka
         });
     }
 
-    @Override
+
     public MisspelledWord getMisspelledWord(int cursorPosition) {
         if (cursorPosition < 0) {
             LOGGER.error("cursorPosition < 0");
@@ -216,7 +238,7 @@ public class SpellCheckTab extends JPanel implements SpellCheckFrame.SpellChecka
         return misspelledMap.keySet().contains(word);
     }
 
-    @Override
+
     public void addMisspelled(MisspelledWord misspelledWord) {
         if (misspelledWord == null) {
             LOGGER.error("misspelledWord is null");
@@ -228,7 +250,7 @@ public class SpellCheckTab extends JPanel implements SpellCheckFrame.SpellChecka
         }
     }
 
-    @Override
+
     public void addOccurance(String mWord, int startIndex) {
         if (mWord == null) {
             LOGGER.error("word is null");
@@ -247,7 +269,7 @@ public class SpellCheckTab extends JPanel implements SpellCheckFrame.SpellChecka
         misspelledMap.get(mWord).addOccurance(mWord, startIndex);
     }
 
-    @Override
+
     public Collection<MisspelledWord> getMisspelled() {
         return Collections.unmodifiableCollection(misspelledMap.values());
     }
@@ -262,45 +284,142 @@ public class SpellCheckTab extends JPanel implements SpellCheckFrame.SpellChecka
         DictionaryService.getInstance().addRankEntry(misspelled, selectedLanguage);
     }
 
-    @Override
     public void highlightMisspelled() {
-        //To change body of implemented methods use File | Settings | File Templates.
+        if (SwingUtilities.isEventDispatchThread()) {
+            EventQueue.invokeLater(new Runnable() {
+                @Override
+                public void run() {
+                    highlight();
+                }
+            });
+        }
+    }
+
+    private void highlight() {
+        LOGGER.info("Removing all highlights");
+        removeAllHighlights();
+        if (misspelledMap.isEmpty()) {
+            return;
+        }
+        for (MisspelledWord misspelledWord : misspelledMap.values()) {
+            for (MisspelledWord.Position position : misspelledWord.getOccurances()) {
+                try {
+                    highlighter.addHighlight(position.getStartIndex(), position.getEndIndex() + 1, painter);
+                } catch (BadLocationException ex) {
+                    LOGGER.error("start: " + position.getStartIndex() + " end:" + position.getEndIndex() + " " + ex.getMessage());
+                }
+            }
+        }
+
+    }
+
+    private void removeAllHighlights() {
+
+        Highlighter.Highlight[] highlights = highlighter.getHighlights();
+        for (Highlighter.Highlight h : highlights) {
+            highlighter.removeHighlight(h);
+        }
     }
 
     private SpellCheckFrame.VisibleText getVisibleText() {
         int offset;
         int length;
 
-        JViewport viewPort = (JViewport) textPane.getParent();
-        offset = textPane.viewToModel(viewPort.getViewRect().getLocation());
+        JViewport viewPort = (JViewport) fileTextPane.getParent();
+        offset = fileTextPane.viewToModel(viewPort.getViewRect().getLocation());
 
         LOGGER.info("offset: " + offset);
         int x = (int) viewPort.getViewRect().getWidth();
         int y = (int) viewPort.getVisibleRect().getHeight();
 
         Point endPoint = new Point(x, y);
-        length = textPane.viewToModel(endPoint);
+        length = fileTextPane.viewToModel(endPoint);
 
         LOGGER.info("length: " + length);
 
-        final int actualLength = textPane.getDocument().getLength();
+        final int actualLength = fileTextPane.getDocument().getLength();
 
         if ((offset + length) > actualLength) {
             length = actualLength - offset;
         }
 
         try {
-            return new SpellCheckFrame.VisibleText(textPane.getText(offset, length), offset);
+            return new SpellCheckFrame.VisibleText(fileTextPane.getText(offset, length), offset);
         } catch (BadLocationException ex) {
-            LOGGER.error(ex.getMessage() + " offset: " + offset + " length: " + length + " text length: " + textPane.getDocument().getLength());
+            LOGGER.error(ex.getMessage() + " offset: " + offset + " length: " + length + " text length: " + fileTextPane.getDocument().getLength());
             throw new IllegalStateException(ex.getMessage());
         }
     }
 
     public void save() {
+        try {
+            fileTextPane.save();
+        } catch (IOException e) {
 
+        }
     }
 
     public void saveAs() {
+        fileTextPane.saveAs();
+    }
+
+    public void open(File file) {
+        try {
+            fileTextPane.open(file);
+        } catch (IOException e) {
+            showMessage("can't open", "Open");
+        }
+    }
+
+    private class UnderlineHighlightPainter extends LayeredHighlighter.LayerPainter {
+
+        private Color color; // The color for the underline
+
+        public UnderlineHighlightPainter(Color c) {
+            if (c == null) {
+                color = Color.RED;
+            }
+            color = c;
+
+        }
+
+        @Override
+        public void paint(Graphics g, int offs0, int offs1, Shape bounds,
+                          JTextComponent c) {
+            // Do nothing: this method will never be called
+        }
+
+        @Override
+        public Shape paintLayer(Graphics g, int offs0, int offs1, Shape bounds,
+                                JTextComponent c, View view) {
+            g.setColor(color == null ? c.getSelectionColor() : color);
+
+            Rectangle alloc = null;
+            if (offs0 == view.getStartOffset() && offs1 == view.getEndOffset()) {
+                if (bounds instanceof Rectangle) {
+                    alloc = (Rectangle) bounds;
+                } else {
+                    alloc = bounds.getBounds();
+                }
+            } else {
+                try {
+                    Shape shape = view.modelToView(offs0,
+                            Position.Bias.Forward, offs1,
+                            Position.Bias.Backward, bounds);
+                    alloc = (shape instanceof Rectangle) ? (Rectangle) shape
+                            : shape.getBounds();
+                } catch (BadLocationException e) {
+                    LOGGER.error("BadLocationException : " + e.getMessage());
+                }
+            }
+
+            FontMetrics fm = c.getFontMetrics(c.getFont());
+            int baseline = alloc.y + alloc.height - fm.getDescent() + 1;
+            g.drawLine(alloc.x, baseline, alloc.x + alloc.width, baseline);
+            g.drawLine(alloc.x, baseline + 1, alloc.x + alloc.width,
+                    baseline + 1);
+
+            return alloc;
+        }
     }
 }
